03cf6b0046812f1a9875441a212b59a27663e443
Sang <sang@mangosango.us>
2013-11-09 13:25:01 -0800

    added activeadmin editor config file back

---
+++ config/initializers/active_admin_editor.rb
@@ -0,0 +1,6 @@
+ActiveAdmin::Editor.configure do |config|
+  # config.s3_bucket = '<your bucket>'
+  # config.aws_access_key_id = '<your aws access key>'
+  # config.aws_access_secret = '<your aws secret>'
+  # config.storage_dir = 'uploads'
+end
