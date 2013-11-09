99dc22bec979b24925550aaa6d481c3896a2bf7b
Sang <sang@mangosango.us>
2013-11-09 12:59:12 -0800

    still crashing..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ DsoWeb::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.initialize_on_precompile = true
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
