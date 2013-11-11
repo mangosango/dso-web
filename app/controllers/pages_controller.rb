f237ddbcb5d7774510e31d5d335622080d9ab030
Sang <sang@mangosango.us>
2013-11-10 18:43:29 -0800

    forgot to change controller code to get latest concert...

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-  	@firstEvent = Concert.order_by(:date => :desc).last
+  	@firstEvent = Concert.order_by(:date => :desc).first
   	@events = Event.order_by(:date => :desc).limit(3)
   end
 end
