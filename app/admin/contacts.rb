8ccbb80b6bd90fdde8dcd599f9f6dda52fd91ce4
Sang <sang@mangosango.us>
2013-11-13 20:44:24 -0800

    more work on contact page. will push more updates soon.

--- app/admin/contacts.rb
+++ app/admin/contacts.rb
@@ -2,8 +2,16 @@ ActiveAdmin.register Contact do
   menu :label => 'Contact'
 
   index :title => "Contact" do
-    column :phone do |contact|
-      link_to contact.phone, edit_admin_contact_path(contact)
+    panel "Contact Info" do
+      table_for Contact.sections do
+        column "section" do |section|
+          section.title
+        end
+      end
     end
+    # column :phone do |contact|
+    #   link_to contact.phone, edit_admin_contact_path(contact)
+    # end
+    # column A
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,5 @@ class Contact
   include Mongoid::Document
   field :phone, type: String
   has_many :sections
+  accepts_nested_attributes_for :sections
 end
