c86b44579f8dc92316f47a6feae24e9545896c13
Sang <sang@mangosango.us>
2013-11-19 00:38:58 -0800

    added contact stuff (finally)

--- app/admin/contacts.rb
+++ app/admin/contacts.rb
@@ -8,4 +8,21 @@ ActiveAdmin.register Contact do
     column :manager_1
     column :manager_2
   end
+
+  form do |f|
+    f.inputs "Contact" do
+      f.input :category
+      f.input :manager_1, :collection => Staff.all.map{ |staff| [staff.name] }
+      f.input :manager_2, :collection => Staff.all.map{ |staff| [staff.name] }
+    end
+    f.actions
+  end
+
+  controller do
+    def update
+      update! do |format|
+        format.html { redirect_to collection_path }
+      end
+    end
+  end
 end
