e00bbf95dbcee931c0004c992284691d148f67a3
Sang <sang@mangosango.us>
2013-11-28 10:26:00 -0800

    changed concert logic to look for the closes concert in the future. if there is no future concert, it just finds the most recent concert.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   def index
-  	@firstEvent = Concert.order_by(:date => :desc).first
+  	@concert = Concert.where(:date.gte => Date.today.to_s).order_by(:date => :asc).first
+  	unless @concert
+  		@concert = Concert.order_by(:date => :desc).first
+  	end
   	@events = Event.order_by(:date => :desc).limit(3)
   end
 end
